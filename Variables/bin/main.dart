67bad4be70101138c3764c2f409bf8d1be0c23a5
imtanbro <tanmay.gangurde@gmail.com>
2020-04-02 14:09:19 +0530

    Folder

---
+++ Variables/bin/main.dart
@@ -0,0 +1,96 @@
+
+void main(List<String> arguments) {
+
+  //variables are the spot in memory or just a bucket to hold value.
+
+  //Data types
+  /*
+    String
+    Number(Int and double)
+    Boolen = 1 or 0, true or false
+    lists maps
+    .....
+  */
+
+//  Relationality operators in dart == != > < >= <=
+
+int num = 34;
+double num1 = 34.0;
+double pi = 3.14;
+double g = 9.8;
+
+print(g>pi);
+
+
+print(num1 == num);
+
+
+
+
+
+  //comcatinate string
+//
+//  String name = 'Tanmay';
+//  String last_name = "Gangurde";
+//
+//  int age = 20;
+//
+//  print("$name $last_name is $age years old");
+//  print("My name is ${name.toUpperCase()}, ${name.toUpperCase()} ${last_name.toUpperCase()}");
+  
+  
+
+
+
+
+
+//  Const and final keyword
+
+//  final pi = 3.14;
+//  const g = 9.98;
+
+
+//  var country = "India";
+//  var pi = 3.14;
+//  pi = 23.89; //so being able to change the value of a variable is not good
+              //and if gthat variable is constant then it can cause many problem in the software
+      //so to avoid this we use the keyword const
+
+  /*
+  Difference betwn contant and final
+    we use const keyword whrn we want the value/variable to be a constant at compile time.
+    and if we want a varable or a valute be be always cont. = never changes we use final
+   */
+
+
+
+//  bool isTrue = true;
+//  bool isFalse = false;
+//
+//  print(isTrue);
+  
+  
+  
+//  num number = 23;
+//  print(number);
+//
+//  int age = 20;
+//  double marks= 73.45;
+//
+//  print("Pring $age and $marks");
+
+
+
+
+
+//
+//  String name;    //this works explicit as we know it will always have a variable which will be of string type
+//  name ="Tmay";
+//
+//  var a = 10;
+//  print(a/3);
+//
+//
+//
+//  print(a);
+}
